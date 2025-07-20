754215e686ba87d443d5bd270d52b2ded662bb9d
Andrey Talanin <andrey.talanin@outlook.com>
2025-07-20 16:02:49 +0200

    Add the WordJsonModel, WordGroupJsonModel JSON-serializable application-level model classes

---
+++ Source/JapaneseLanguageTools.Contracts.Json/Models/WordGroup.JsonModel.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Text.Json.Serialization;
+
+using JapaneseLanguageTools.Contracts.Enumerations;
+
+// Use the IDE0079 (Remove unnecessary suppression) suppression (a Visual Studio false positive).
+// Disable the IDE0130 (Namespace does not match folder structure) notification to preserve namespace structure.
+#pragma warning disable IDE0079
+#pragma warning disable IDE0130
+#pragma warning restore IDE0079
+
+namespace JapaneseLanguageTools.Contracts.Models.Json;
+
+public class WordGroupJsonModel
+{
+    [JsonPropertyName("id")]
+    public int? Id { get; set; }
+
+    [JsonPropertyName("action")]
+    public SnapshotObjectAction Action { get; set; }
+
+    [JsonPropertyName("caption")]
+    public string Caption { get; set; } = string.Empty;
+
+    [JsonPropertyName("comment")]
+    public string? Comment { get; set; }
+
+    [JsonPropertyName("enabled")]
+    public bool Enabled { get; set; }
+
+    [JsonPropertyName("alwaysUse")]
+    public bool AlwaysUse { get; set; }
+
+    [JsonPropertyName("hidden")]
+    public bool Hidden { get; set; }
+
+    [JsonPropertyName("words")]
+    public WordJsonModel[] Words { get; set; } = [];
+
+    [JsonPropertyName("createdOn")]
+    public DateTimeOffset CreatedOn { get; set; }
+
+    [JsonPropertyName("updatedOn")]
+    public DateTimeOffset UpdatedOn { get; set; }
+}
