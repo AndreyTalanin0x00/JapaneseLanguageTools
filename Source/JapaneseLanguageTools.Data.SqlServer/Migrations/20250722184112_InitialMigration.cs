691ba92bd88eeb00e639836ae35bc63f50162339
Andrey Talanin <andrey.talanin@outlook.com>
2025-07-22 20:44:43 +0200

    Add the InitialMigration SqlServer database migration class

---
+++ Source/JapaneseLanguageTools.Data.SqlServer/Migrations/20250722184112_InitialMigration.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JapaneseLanguageTools.Data.SqlServer.Migrations;
+
+public partial class InitialMigration : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
