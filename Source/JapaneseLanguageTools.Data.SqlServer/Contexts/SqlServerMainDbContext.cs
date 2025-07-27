a82137ab21429dbb50f11803f33f3fbb5bb08926
Andrey Talanin <andrey.talanin@outlook.com>
2025-07-27 17:15:38 +0200

    Add the Tags database set to the SqlServerMainDbContext database context class

--- Source/JapaneseLanguageTools.Data.SqlServer/Contexts/SqlServerMainDbContext.cs
+++ Source/JapaneseLanguageTools.Data.SqlServer/Contexts/SqlServerMainDbContext.cs
@@ -1,4 +1,5 @@
 using JapaneseLanguageTools.Data.Contexts;
+using JapaneseLanguageTools.Data.Entities;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,6 +21,31 @@ public class SqlServerMainDbContext : MainDbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Tag>(entityBuilder =>
+        {
+            entityBuilder.HasKey(entity => entity.Id);
+
+            entityBuilder.HasAlternateKey(entity => entity.Caption);
+
+            entityBuilder
+                .HasIndex(entity => entity.Caption)
+                .HasDatabaseName("UIX_Tag_Caption")
+                .IsUnique();
+
+            entityBuilder
+                .Property(entity => entity.CreatedOn)
+                .ValueGeneratedOnAdd();
+            entityBuilder
+                .Property(entity => entity.UpdatedOn)
+                .ValueGeneratedOnAddOrUpdate();
+
+            entityBuilder.ToTable("Tag", "dbo", tableBuilder =>
+            {
+                tableBuilder.HasCheckConstraint("CK_Tag_Caption_NotEmpty", "LEN(TRIM([Caption])) > 0");
+                tableBuilder.HasCheckConstraint("CK_Tag_PlaceholderMarker_NullOrNotEmpty", "[PlaceholderMarker] IS NULL OR LEN(TRIM([PlaceholderMarker])) > 0");
+            });
+        });
+
         base.OnModelCreating(modelBuilder);
     }
 }
