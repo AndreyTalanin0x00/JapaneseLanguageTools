4e5a5d3c55f024a028ef973872201bc124cf8320
Andrey Talanin <andrey.talanin@outlook.com>
2025-07-27 19:48:53 +0200

    Add the CharacterMigration Sqlite database migration class

--- Source/JapaneseLanguageTools.Data.Sqlite/Migrations/20250727174604_CharacterMigration.Designer.cs
+++ Source/JapaneseLanguageTools.Data.Sqlite/Migrations/20250727174604_CharacterMigration.Designer.cs
@@ -3,6 +3,7 @@ using JapaneseLanguageTools.Data.Sqlite.Contexts;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JapaneseLanguageTools.Data.Sqlite.Migrations
 {
     [DbContext(typeof(SqliteMainDbContext))]
-    partial class SqliteMainDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250727174604_CharacterMigration")]
+    partial class CharacterMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.7");
