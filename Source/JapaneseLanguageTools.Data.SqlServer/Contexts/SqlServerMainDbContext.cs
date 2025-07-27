56ae3d38ce5f62ac0005d0e80362a1cef3fb86d4
Andrey Talanin <andrey.talanin@outlook.com>
2025-07-27 17:20:29 +0200

    Add the Characters, CharacterGroups, CharacterTags database sets to the SqlServerMainDbContext database context class

--- Source/JapaneseLanguageTools.Data.SqlServer/Contexts/SqlServerMainDbContext.cs
+++ Source/JapaneseLanguageTools.Data.SqlServer/Contexts/SqlServerMainDbContext.cs
@@ -46,6 +46,90 @@ public class SqlServerMainDbContext : MainDbContext
             });
         });
 
+        modelBuilder.Entity<Character>(entityBuilder =>
+        {
+            entityBuilder.HasKey(entity => entity.Id);
+
+            entityBuilder
+                .HasOne(entity => entity.CharacterGroup)
+                .WithMany(entity => entity.Characters)
+                .HasForeignKey(entity => entity.CharacterGroupId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entityBuilder
+                .HasIndex(entity => entity.CharacterGroupId)
+                .HasDatabaseName("IX_Character_CharacterGroupId");
+
+            entityBuilder
+                .Property(entity => entity.CreatedOn)
+                .ValueGeneratedOnAdd();
+            entityBuilder
+                .Property(entity => entity.UpdatedOn)
+                .ValueGeneratedOnAddOrUpdate();
+
+            entityBuilder.ToTable("Character", "dbo", tableBuilder =>
+            {
+                tableBuilder.HasCheckConstraint("CK_Character_Symbol_NotEmpty", "LEN(TRIM([Symbol])) > 0");
+                tableBuilder.HasCheckConstraint("CK_Character_Pronunciation_NullOrNotEmpty", "[Pronunciation] IS NULL OR LEN(TRIM([Pronunciation])) > 0");
+                tableBuilder.HasCheckConstraint("CK_Character_Syllable_NullOrNotEmpty", "[Syllable] IS NULL OR LEN(TRIM([Syllable])) > 0");
+                tableBuilder.HasCheckConstraint("CK_Character_Onyomi_NullOrNotEmpty", "[Onyomi] IS NULL OR LEN(TRIM([Onyomi])) > 0");
+                tableBuilder.HasCheckConstraint("CK_Character_Kunyomi_NullOrNotEmpty", "[Kunyomi] IS NULL OR LEN(TRIM([Kunyomi])) > 0");
+                tableBuilder.HasCheckConstraint("CK_Character_Meaning_NullOrNotEmpty", "[Meaning] IS NULL OR LEN(TRIM([Meaning])) > 0");
+            });
+        });
+
+        modelBuilder.Entity<CharacterGroup>(entityBuilder =>
+        {
+            entityBuilder.HasKey(entity => entity.Id);
+
+            entityBuilder.HasAlternateKey(entity => entity.Caption);
+
+            entityBuilder
+                .HasIndex(entity => entity.Caption)
+                .HasDatabaseName("UIX_CharacterGroup_Caption")
+                .IsUnique();
+
+            entityBuilder
+                .Property(entity => entity.CreatedOn)
+                .ValueGeneratedOnAdd();
+            entityBuilder
+                .Property(entity => entity.UpdatedOn)
+                .ValueGeneratedOnAddOrUpdate();
+
+            entityBuilder.ToTable("CharacterGroup", "dbo", tableBuilder =>
+            {
+                tableBuilder.HasCheckConstraint("CK_CharacterGroup_Caption_NotEmpty", "LEN(TRIM([Caption])) > 0");
+                tableBuilder.HasCheckConstraint("CK_CharacterGroup_Comment_NullOrNotEmpty", "[Comment] IS NULL OR LEN(TRIM([Comment])) > 0");
+            });
+        });
+
+        modelBuilder.Entity<CharacterTag>(entityBuilder =>
+        {
+            entityBuilder.HasKey(entity => new { entity.CharacterId, entity.TagId });
+
+            entityBuilder
+                .HasOne(entity => entity.Character)
+                .WithMany(entity => entity.CharacterTags)
+                .HasForeignKey(entity => entity.CharacterId)
+                .OnDelete(DeleteBehavior.Cascade);
+            entityBuilder
+                .HasOne(entity => entity.Tag)
+                .WithMany()
+                .HasForeignKey(entity => entity.TagId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entityBuilder
+                .HasIndex(entity => entity.CharacterId)
+                .HasDatabaseName("IX_CharacterTag_CharacterId");
+            entityBuilder
+                .HasIndex(entity => entity.TagId)
+                .HasDatabaseName("IX_CharacterTag_TagId");
+
+            entityBuilder.ToTable("CharacterTag", "dbo", tableBuilder =>
+            {
+            });
+        });
+
         base.OnModelCreating(modelBuilder);
     }
 }
