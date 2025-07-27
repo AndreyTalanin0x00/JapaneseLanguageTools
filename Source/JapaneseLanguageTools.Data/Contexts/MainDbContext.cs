5adaf8243d95463a7a4ac6c7e46db912232699c6
Andrey Talanin <andrey.talanin@outlook.com>
2025-07-27 16:36:17 +0200

    Add the Tags database set to the MainDbContext database context class

--- Source/JapaneseLanguageTools.Data/Contexts/MainDbContext.cs
+++ Source/JapaneseLanguageTools.Data/Contexts/MainDbContext.cs
@@ -1,16 +1,22 @@
+using JapaneseLanguageTools.Data.Entities;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace JapaneseLanguageTools.Data.Contexts;
 
 public class MainDbContext : DbContext
 {
+    public DbSet<Tag> Tags { get; }
+
     protected MainDbContext()
         : base()
     {
+        Tags = Set<Tag>();
     }
 
     protected MainDbContext(DbContextOptions contextOptions)
         : base(contextOptions)
     {
+        Tags = Set<Tag>();
     }
 }
