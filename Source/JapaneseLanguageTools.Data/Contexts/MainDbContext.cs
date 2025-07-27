21b60ba060ab48da6b5a122a365b2ac059558502
Andrey Talanin <andrey.talanin@outlook.com>
2025-07-27 16:37:30 +0200

    Add the Characters, CharacterGroups, CharacterTags database sets to the MainDbContext database context class

--- Source/JapaneseLanguageTools.Data/Contexts/MainDbContext.cs
+++ Source/JapaneseLanguageTools.Data/Contexts/MainDbContext.cs
@@ -6,17 +6,29 @@ namespace JapaneseLanguageTools.Data.Contexts;
 
 public class MainDbContext : DbContext
 {
+    public DbSet<Character> Characters { get; }
+
+    public DbSet<CharacterGroup> CharacterGroups { get; }
+
+    public DbSet<CharacterTag> CharacterTags { get; }
+
     public DbSet<Tag> Tags { get; }
 
     protected MainDbContext()
         : base()
     {
+        Characters = Set<Character>();
+        CharacterGroups = Set<CharacterGroup>();
+        CharacterTags = Set<CharacterTag>();
         Tags = Set<Tag>();
     }
 
     protected MainDbContext(DbContextOptions contextOptions)
         : base(contextOptions)
     {
+        Characters = Set<Character>();
+        CharacterGroups = Set<CharacterGroup>();
+        CharacterTags = Set<CharacterTag>();
         Tags = Set<Tag>();
     }
 }
