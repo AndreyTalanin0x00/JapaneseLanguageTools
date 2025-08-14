5c3408f3acb42d72366cd77807bd4cd28f54d591
Andrey Talanin <andrey.talanin@outlook.com>
2025-08-14 23:03:53 +0200

    Add the SubmitCompletedCharacterExerciseDetailsResponseModel application-level response model class

---
+++ Source/JapaneseLanguageTools.Contracts/Models/Responses/SubmitCompletedCharacterExerciseDetailsResponse.Model.cs
@@ -0,0 +1,8 @@
+using JapaneseLanguageTools.Contracts.Models.Requests;
+
+namespace JapaneseLanguageTools.Contracts.Models.Responses;
+
+public class SubmitCompletedCharacterExerciseDetailsResponseModel
+{
+    public required SubmitCompletedCharacterExerciseDetailsRequestModel Request { get; set; }
+}
