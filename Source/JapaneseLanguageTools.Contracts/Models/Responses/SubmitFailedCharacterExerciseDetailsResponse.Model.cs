3addb209732d99c8684f517bba7197ebd4a84d10
Andrey Talanin <andrey.talanin@outlook.com>
2025-08-14 23:04:18 +0200

    Add the SubmitFailedCharacterExerciseDetailsResponseModel application-level response model class

---
+++ Source/JapaneseLanguageTools.Contracts/Models/Responses/SubmitFailedCharacterExerciseDetailsResponse.Model.cs
@@ -0,0 +1,8 @@
+using JapaneseLanguageTools.Contracts.Models.Requests;
+
+namespace JapaneseLanguageTools.Contracts.Models.Responses;
+
+public class SubmitFailedCharacterExerciseDetailsResponseModel
+{
+    public required SubmitFailedCharacterExerciseDetailsRequestModel Request { get; set; }
+}
