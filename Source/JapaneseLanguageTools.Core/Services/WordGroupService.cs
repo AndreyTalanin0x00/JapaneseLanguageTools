79f51a41ad103a9439ddb2e6ac9c442b05735a54
Andrey Talanin <andrey.talanin@outlook.com>
2025-08-23 15:52:46 +0200

    Implement the WordService, WordGroupService service classes

--- Source/JapaneseLanguageTools.Core/Services/WordGroupService.cs
+++ Source/JapaneseLanguageTools.Core/Services/WordGroupService.cs
@@ -1,68 +1,164 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
+using AndreyTalanin0x00.YieldHelpers;
+
+using AutoMapper;
 
 using JapaneseLanguageTools.Contracts.Identifiers;
 using JapaneseLanguageTools.Contracts.Models;
 using JapaneseLanguageTools.Contracts.Services.Abstractions;
+using JapaneseLanguageTools.Data.Entities;
+using JapaneseLanguageTools.Data.Repositories.Abstractions;
 
 namespace JapaneseLanguageTools.Core.Services;
 
 public class WordGroupService : IWordGroupService
 {
-    public WordGroupService()
+    private readonly IMapper m_mapper;
+    private readonly IWordGroupRepository m_wordGroupRepository;
+    private readonly TimeProvider m_timeProvider;
+
+    public WordGroupService(IMapper mapper, IWordGroupRepository wordGroupRepository, TimeProvider timeProvider)
     {
-        throw new NotImplementedException();
+        m_mapper = mapper;
+        m_wordGroupRepository = wordGroupRepository;
+        m_timeProvider = timeProvider;
     }
 
-    public Task<WordGroupModel?> GetWordGroupAsync(WordGroupId wordGroupId, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<WordGroupModel?> GetWordGroupAsync(WordGroupId wordGroupId, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        WordGroupId[] wordGroupIds = [wordGroupId];
+
+        WordGroupModel? wordGroupModel = (await GetWordGroupsAsync(wordGroupIds, cancellationToken)).SingleOrDefault();
+
+        return wordGroupModel;
     }
 
-    public Task<WordGroupModel?> GetWordGroupAsync(WordGroupCaption wordGroupCaption, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<WordGroupModel?> GetWordGroupAsync(WordGroupCaption wordGroupCaption, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        WordGroupCaption[] wordGroupCaptions = [wordGroupCaption];
+
+        WordGroupModel? wordGroupModel = (await GetWordGroupsAsync(wordGroupCaptions, cancellationToken)).SingleOrDefault();
+
+        return wordGroupModel;
     }
 
-    public Task<WordGroupModel[]> GetWordGroupsAsync(IEnumerable<WordGroupId> wordGroupIds, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<WordGroupModel[]> GetWordGroupsAsync(IEnumerable<WordGroupId> wordGroupIds, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        WordGroup[] wordGroups = await m_wordGroupRepository.GetWordGroupsAsync(wordGroupIds, cancellationToken);
+        if (wordGroups.Length == 0)
+            return [];
+
+        WordGroupModel[] wordGroupModels = m_mapper.Map<WordGroupModel[]>(wordGroups);
+
+        RemoveNavigationPropertyCycles(wordGroupModels);
+
+        return wordGroupModels;
     }
 
-    public Task<WordGroupModel[]> GetWordGroupsAsync(IEnumerable<WordGroupCaption> wordGroupCaptions, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<WordGroupModel[]> GetWordGroupsAsync(IEnumerable<WordGroupCaption> wordGroupCaptions, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        WordGroup[] wordGroups = await m_wordGroupRepository.GetWordGroupsAsync(wordGroupCaptions, cancellationToken);
+        if (wordGroups.Length == 0)
+            return [];
+
+        WordGroupModel[] wordGroupModels = m_mapper.Map<WordGroupModel[]>(wordGroups);
+
+        RemoveNavigationPropertyCycles(wordGroupModels);
+
+        return wordGroupModels;
     }
 
-    public Task<WordGroupModel[]> GetAllWordGroupsAsync(CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<WordGroupModel[]> GetAllWordGroupsAsync(CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        WordGroup[] wordGroups = await m_wordGroupRepository.GetAllWordGroupsAsync(cancellationToken);
+        if (wordGroups.Length == 0)
+            return [];
+
+        WordGroupModel[] wordGroupModels = m_mapper.Map<WordGroupModel[]>(wordGroups);
+
+        RemoveNavigationPropertyCycles(wordGroupModels);
+
+        return wordGroupModels;
     }
 
-    public Task<WordGroupModel> AddWordGroupAsync(WordGroupModel wordGroupModel, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<WordGroupModel> AddWordGroupAsync(WordGroupModel wordGroupModel, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        WordGroup wordGroup = m_mapper.Map<WordGroup>(wordGroupModel);
+
+        DateTimeOffset utcNow = m_timeProvider.GetUtcNow();
+        wordGroup.CreatedOn = utcNow;
+        wordGroup.UpdatedOn = utcNow;
+
+        WordGroup addedWordGroup = await m_wordGroupRepository.AddWordGroupAsync(wordGroup, cancellationToken);
+
+        WordGroupModel addedWordGroupModel = m_mapper.Map<WordGroupModel>(addedWordGroup);
+
+        RemoveNavigationPropertyCycles(YieldEnumerableHelpers.Yield(addedWordGroupModel));
+
+        return addedWordGroupModel;
     }
 
-    public Task<bool> UpdateWordGroupAsync(WordGroupModel wordGroupModel, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<bool> UpdateWordGroupAsync(WordGroupModel wordGroupModel, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        WordGroup wordGroup = m_mapper.Map<WordGroup>(wordGroupModel);
+
+        DateTimeOffset utcNow = m_timeProvider.GetUtcNow();
+        wordGroup.UpdatedOn = utcNow;
+
+        bool updated = await m_wordGroupRepository.UpdateWordGroupAsync(wordGroup, cancellationToken);
+
+        return updated;
     }
 
-    public Task<bool> ChangeWordGroupStateAsync(WordGroupModel wordGroupModel, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<bool> ChangeWordGroupStateAsync(WordGroupModel wordGroupModel, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        WordGroup wordGroup = m_mapper.Map<WordGroup>(wordGroupModel);
+
+        DateTimeOffset utcNow = m_timeProvider.GetUtcNow();
+        wordGroup.UpdatedOn = utcNow;
+
+        bool stateChanged = await m_wordGroupRepository.ChangeWordGroupStateAsync(wordGroup, cancellationToken);
+
+        return stateChanged;
     }
 
-    public Task<bool> RemoveWordGroupAsync(WordGroupId wordGroupId, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<bool> RemoveWordGroupAsync(WordGroupId wordGroupId, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        bool removed = await m_wordGroupRepository.RemoveWordGroupAsync(wordGroupId, cancellationToken);
+
+        return removed;
     }
 
-    public Task<bool> RemoveWordGroupAsync(WordGroupCaption wordGroupCaption, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<bool> RemoveWordGroupAsync(WordGroupCaption wordGroupCaption, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        bool removed = await m_wordGroupRepository.RemoveWordGroupAsync(wordGroupCaption, cancellationToken);
+
+        return removed;
+    }
+
+    protected static void RemoveNavigationPropertyCycles(IEnumerable<WordGroupModel> wordGroupModels)
+    {
+        foreach (WordGroupModel wordGroupModel in wordGroupModels)
+        {
+            foreach (WordModel wordModel in wordGroupModel.Words)
+            {
+                wordModel.WordGroup = null;
+            }
+        }
     }
 }
--- Source/JapaneseLanguageTools.Core/Services/WordService.cs
+++ Source/JapaneseLanguageTools.Core/Services/WordService.cs
@@ -1,48 +1,119 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
+using AndreyTalanin0x00.YieldHelpers;
+
+using AutoMapper;
 
 using JapaneseLanguageTools.Contracts.Identifiers;
 using JapaneseLanguageTools.Contracts.Models;
 using JapaneseLanguageTools.Contracts.Services.Abstractions;
+using JapaneseLanguageTools.Data.Entities;
+using JapaneseLanguageTools.Data.Repositories.Abstractions;
 
 namespace JapaneseLanguageTools.Core.Services;
 
 public class WordService : IWordService
 {
-    public WordService()
+    private readonly IMapper m_mapper;
+    private readonly IWordRepository m_wordRepository;
+    private readonly TimeProvider m_timeProvider;
+
+    public WordService(IMapper mapper, IWordRepository wordRepository, TimeProvider timeProvider)
     {
-        throw new NotImplementedException();
+        m_mapper = mapper;
+        m_wordRepository = wordRepository;
+        m_timeProvider = timeProvider;
     }
 
-    public Task<WordModel?> GetWordAsync(WordId wordId, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<WordModel?> GetWordAsync(WordId wordId, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        WordId[] wordIds = [wordId];
+
+        WordModel? wordModel = (await GetWordsAsync(wordIds, cancellationToken)).SingleOrDefault();
+
+        return wordModel;
     }
 
-    public Task<WordModel[]> GetWordsAsync(IEnumerable<WordId> wordIds, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<WordModel[]> GetWordsAsync(IEnumerable<WordId> wordIds, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        Word[] words = await m_wordRepository.GetWordsAsync(wordIds, cancellationToken);
+        if (words.Length == 0)
+            return [];
+
+        WordModel[] wordModels = m_mapper.Map<WordModel[]>(words);
+
+        RemoveNavigationPropertyCycles(wordModels);
+
+        return wordModels;
     }
 
-    public Task<WordModel[]> GetAllWordsAsync(CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<WordModel[]> GetAllWordsAsync(CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        Word[] words = await m_wordRepository.GetAllWordsAsync(cancellationToken);
+        if (words.Length == 0)
+            return [];
+
+        WordModel[] wordModels = m_mapper.Map<WordModel[]>(words);
+
+        RemoveNavigationPropertyCycles(wordModels);
+
+        return wordModels;
     }
 
-    public Task<WordModel> AddWordAsync(WordModel wordModel, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<WordModel> AddWordAsync(WordModel wordModel, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        Word word = m_mapper.Map<Word>(wordModel);
+
+        DateTimeOffset utcNow = m_timeProvider.GetUtcNow();
+        word.CreatedOn = utcNow;
+        word.UpdatedOn = utcNow;
+
+        Word addedWord = await m_wordRepository.AddWordAsync(word, cancellationToken);
+
+        WordModel addedWordModel = m_mapper.Map<WordModel>(addedWord);
+
+        RemoveNavigationPropertyCycles(YieldEnumerableHelpers.Yield(addedWordModel));
+
+        return addedWordModel;
     }
 
-    public Task<bool> UpdateWordAsync(WordModel wordModel, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<bool> UpdateWordAsync(WordModel wordModel, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        Word word = m_mapper.Map<Word>(wordModel);
+
+        DateTimeOffset utcNow = m_timeProvider.GetUtcNow();
+        word.UpdatedOn = utcNow;
+
+        bool updated = await m_wordRepository.UpdateWordAsync(word, cancellationToken);
+
+        return updated;
     }
 
-    public Task<bool> RemoveWordAsync(WordId wordId, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task<bool> RemoveWordAsync(WordId wordId, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        bool removed = await m_wordRepository.RemoveWordAsync(wordId, cancellationToken);
+
+        return removed;
+    }
+
+    protected static void RemoveNavigationPropertyCycles(IEnumerable<WordModel> wordModels)
+    {
+        foreach (WordModel wordModel in wordModels)
+        {
+            if (wordModel.WordGroup?.Words.Count > 0)
+            {
+                wordModel.WordGroup.Words = [];
+            }
+        }
     }
 }
