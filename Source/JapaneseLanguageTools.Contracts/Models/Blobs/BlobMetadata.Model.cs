012b901caf410883831629383c6d2cb44ecd5a25
Andrey Talanin <andrey.talanin@outlook.com>
2026-03-29 18:01:27 +0200

    Add the BlobMetadataModel application-level model class

---
+++ Source/JapaneseLanguageTools.Contracts/Models/Blobs/BlobMetadata.Model.cs
@@ -0,0 +1,8 @@
+namespace JapaneseLanguageTools.Contracts.Models.Blobs;
+
+public class BlobMetadataModel
+{
+    public required string MimeType { get; set; }
+
+    public required string FileName { get; set; }
+}
