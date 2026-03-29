14ce816a287297f4f8fee679fb487ec667a42336
Andrey Talanin <andrey.talanin@outlook.com>
2026-03-30 00:31:22 +0200

    Add the BlobBucketConstants static class

---
+++ Source/JapaneseLanguageTools.Core.Blobs/Constants/BlobBucketConstants.cs
@@ -0,0 +1,12 @@
+namespace JapaneseLanguageTools.Core.Blobs.Constants;
+
+public static class BlobBucketConstants
+{
+    public static readonly string ExportBlobs = "export-blobs";
+
+    public static readonly string ExportIntermediateBlobs = "export-intermediate-blobs";
+
+    public static readonly string ImportBlobs = "import-blobs";
+
+    public static readonly string ImportIntermediateBlobs = "import-intermediate-blobs";
+}
