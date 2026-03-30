50b65c3d12457851bf53fd06ddf3cfa2a8a0352f
Andrey Talanin <andrey.talanin@outlook.com>
2026-03-31 01:14:23 +0200

    Add the ApplicationDictionaryExportStreamEnabledService, TagExportStreamEnabledService placeholder service implementation classes

---
+++ Source/JapaneseLanguageTools.Core.Export/Services/X-TagExport-Implementations/TagExportService.StreamEnabled.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+using AndreyTalanin0x00.Integrations.Blobs;
+
+using JapaneseLanguageTools.Contracts.Models.Blobs.Requests;
+using JapaneseLanguageTools.Contracts.Models.Blobs.Responses;
+using JapaneseLanguageTools.Contracts.Models.Requests;
+using JapaneseLanguageTools.Contracts.Models.Responses;
+using JapaneseLanguageTools.Core.Services.Abstractions;
+
+// Use the IDE0079 (Remove unnecessary suppression) suppression (a Visual Studio false positive).
+// Disable the IDE0130 (Namespace does not match folder structure) notification to preserve namespace structure.
+#pragma warning disable IDE0079
+#pragma warning disable IDE0130
+#pragma warning restore IDE0079
+
+namespace JapaneseLanguageTools.Core.Export.Services;
+
+public class TagExportStreamEnabledService : ITagExportStreamEnabledService
+{
+    /// <inheritdoc />
+    public Task<ExportTagsResponseModel> ExportTagsAsync(ExportTagsRequestModel exportTagsRequestModel, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task<GetBlobMetadataResponseModel?> GetExportBlobMetadataAsync(GetBlobMetadataRequestModel getBlobMetadataRequestModel, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task<GetBlobExpirationTimeResponseModel?> GetExportBlobExpirationTimeAsync(GetBlobExpirationTimeRequestModel getBlobExpirationTimeRequestModel, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task<BlobStreamMetadataPair<Stream, DownloadBlobResponseModel>?> DownloadExportBlobAsync(DownloadBlobRequestModel downloadBlobRequestModel, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
