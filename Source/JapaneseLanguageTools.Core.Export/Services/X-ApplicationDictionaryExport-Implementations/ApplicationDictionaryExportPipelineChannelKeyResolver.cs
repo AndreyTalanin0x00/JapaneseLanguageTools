d36601ad48183d1b31156ddd1509c2865a1c8e29
Andrey Talanin <andrey.talanin@outlook.com>
2026-03-31 01:13:18 +0200

    Add the ApplicationDictionaryExportPipelineChannelKeyResolver, TagExportPipelineChannelKeyResolver placeholder service implementation classes

---
+++ Source/JapaneseLanguageTools.Core.Export/Services/X-ApplicationDictionaryExport-Implementations/ApplicationDictionaryExportPipelineChannelKeyResolver.cs
@@ -0,0 +1,25 @@
+using System;
+
+using AndreyTalanin0x00.Integrations.Export;
+using AndreyTalanin0x00.Integrations.Export.Services.Abstractions;
+
+using JapaneseLanguageTools.Core.Export.Requests;
+using JapaneseLanguageTools.Core.Export.Responses;
+
+// Use the IDE0079 (Remove unnecessary suppression) suppression (a Visual Studio false positive).
+// Disable the IDE0130 (Namespace does not match folder structure) notification to preserve namespace structure.
+#pragma warning disable IDE0079
+#pragma warning disable IDE0130
+#pragma warning restore IDE0079
+
+namespace JapaneseLanguageTools.Core.Export.Services;
+
+public class ApplicationDictionaryExportPipelineChannelKeyResolver :
+    IExportPipelineChannelKeyResolver<ApplicationDictionaryExportRequest, ApplicationDictionaryExportResponse>
+{
+    /// <inheritdoc />
+    public ExportPipelineChannelKey ResolveExportPipelineChannelKey(ApplicationDictionaryExportRequest applicationDictionaryExportRequest)
+    {
+        throw new NotImplementedException();
+    }
+}
