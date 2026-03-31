a30fdcf40b77414e6150e2eb7ca1966bb2527f67
Andrey Talanin <andrey.talanin@outlook.com>
2026-03-31 20:21:14 +0200

    Add the ApplicationDictionaryImportReader, TagImportReader placeholder service implementation classes

---
+++ Source/JapaneseLanguageTools.Core.Import/Services/X-TagImport-Implementations/TagImportReader.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using AndreyTalanin0x00.Integrations.Import;
+using AndreyTalanin0x00.Integrations.Import.Services.Abstractions;
+
+using JapaneseLanguageTools.Core.Import.Requests;
+using JapaneseLanguageTools.Core.Import.Responses;
+
+// Use the IDE0079 (Remove unnecessary suppression) suppression (a Visual Studio false positive).
+// Disable the IDE0130 (Namespace does not match folder structure) notification to preserve namespace structure.
+#pragma warning disable IDE0079
+#pragma warning disable IDE0130
+#pragma warning restore IDE0079
+
+namespace JapaneseLanguageTools.Core.Import.Services;
+
+public class TagImportReader :
+    IImportReader<TagImportRequest, TagImportResponse>
+{
+    /// <inheritdoc />
+    public Task<ImportSourceBatch[]> ReadAsync(TagImportRequest tagImportRequest, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
