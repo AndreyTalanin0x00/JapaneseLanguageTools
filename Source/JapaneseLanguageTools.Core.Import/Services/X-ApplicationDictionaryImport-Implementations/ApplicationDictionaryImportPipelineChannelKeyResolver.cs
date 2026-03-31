7a4b52b86870191462dfd81f7763bb7846047b4c
Andrey Talanin <andrey.talanin@outlook.com>
2026-03-31 20:31:59 +0200

    Add the ApplicationDictionaryImportPipelineChannelKeyResolver, TagImportPipelineChannelKeyResolver placeholder service implementation classes

---
+++ Source/JapaneseLanguageTools.Core.Import/Services/X-ApplicationDictionaryImport-Implementations/ApplicationDictionaryImportPipelineChannelKeyResolver.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using AndreyTalanin0x00.Integrations.Import;
+using AndreyTalanin0x00.Integrations.Import.Services.Abstractions;
+
+using JapaneseLanguageTools.Core.Import.Requests;
+using JapaneseLanguageTools.Core.Import.Responses;
+
+// Use the IDE0079 (Remove unnecessary suppression) suppression (a Visual Studio false positive).
+// Disable the IDE0130 (Namespace does not match folder structure) notification to preserve namespace structure.
+#pragma warning disable IDE0079
+#pragma warning disable IDE0130
+#pragma warning restore IDE0079
+
+namespace JapaneseLanguageTools.Core.Import.Services;
+
+public class ApplicationDictionaryImportPipelineChannelKeyResolver :
+    IImportPipelineChannelKeyResolver<ApplicationDictionaryImportRequest, ApplicationDictionaryImportResponse>
+{
+    /// <inheritdoc />
+    public bool SupportsAsyncMode => throw new NotImplementedException();
+
+    /// <inheritdoc />
+    public ImportPipelineChannelKey ResolveImportPipelineChannelKey(ApplicationDictionaryImportRequest applicationDictionaryImportRequest, ImportSource importSource, ImportSourceContext importSourceContext)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task<ImportPipelineChannelKey> ResolveImportPipelineChannelKeyAsync(ApplicationDictionaryImportRequest applicationDictionaryImportRequest, ImportSource importSource, ImportSourceContext importSourceContext, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
