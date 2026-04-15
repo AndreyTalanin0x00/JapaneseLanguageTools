28859ee1163fc8832c889ec6158ebc5bff7f2e55
Andrey Talanin <andrey.talanin@outlook.com>
2026-04-15 23:21:35 +0200

    Update the CharacterTypes TypeScript enumeration (add helper functions)

--- Source/JapaneseLanguageTools.ReactSpa/source/enumerations/CharacterTypes.ts
+++ Source/JapaneseLanguageTools.ReactSpa/source/enumerations/CharacterTypes.ts
@@ -10,3 +10,31 @@ enum CharacterTypes {
 }
 
 export default CharacterTypes;
+
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class CharacterTypesUtils {
+  public static characterTypesToString(characterTypes: CharacterTypes): string {
+    if (characterTypes == CharacterTypes.None) {
+      return CharacterTypes[CharacterTypes.None];
+    }
+
+    const compositeValues = [CharacterTypes.AllExceptKanji, CharacterTypes.All];
+    for (const compositeValue of compositeValues) {
+      if (characterTypes == compositeValue) {
+        return CharacterTypes[compositeValue];
+      }
+    }
+
+    const resultValues = [];
+    const atomicValues = [CharacterTypes.Hiragana, CharacterTypes.Katakana, CharacterTypes.Kanji];
+    for (const atomicValue of atomicValues) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
+      if ((atomicValue & characterTypes) == atomicValue) {
+        resultValues.push(CharacterTypes[atomicValue]);
+      }
+    }
+
+    const resultString = resultValues.join(", ");
+    return resultString;
+  }
+}
