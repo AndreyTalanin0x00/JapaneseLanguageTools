c1e624cdff5150cbcd058e434c50c4470411c62e
Andrey Talanin <andrey.talanin@outlook.com>
2026-04-16 00:50:25 +0200

    Add the ExportApplicationDictionaryRequestModel, ExportTagsRequestModel TypeScript request model interfaces

---
+++ Source/JapaneseLanguageTools.ReactSpa/source/models/requests/x-export/ExportTagsRequest.Model.ts
@@ -0,0 +1,4 @@
+import ExportRequestModel from "@/models/requests/base/ExportRequest.Model";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export default interface ExportTagsRequestModel extends ExportRequestModel {}
