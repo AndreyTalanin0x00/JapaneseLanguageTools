2e9356545312300693551702506e7fada280a964
Andrey Talanin <andrey.talanin@outlook.com>
2026-04-15 23:27:42 +0200

    Add the CharacterProperties TypeScript enumeration

---
+++ Source/JapaneseLanguageTools.ReactSpa/source/enumerations/CharacterProperties.ts
@@ -0,0 +1,12 @@
+enum CharacterProperties {
+  None = 0x00,
+  Type = 0x01,
+  Symbol = 0x02,
+  Onyomi = 0x04,
+  Kunyomi = 0x08,
+  Pronunciation = 0x10,
+  Syllable = 0x20,
+  Meaning = 0x40,
+}
+
+export default CharacterProperties;
