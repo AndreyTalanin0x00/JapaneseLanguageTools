5abbc5e2fcee5d938b89f5f99d9873cc7a904eb0
Andrey Talanin <andrey.talanin@outlook.com>
2026-04-15 23:28:19 +0200

    Add the WordProperties TypeScript enumeration

---
+++ Source/JapaneseLanguageTools.ReactSpa/source/enumerations/WordProperties.ts
@@ -0,0 +1,11 @@
+enum WordProperties {
+  None = 0x00,
+  Characters = 0x01,
+  CharacterTypes = 0x02,
+  Pronunciation = 0x04,
+  Furigana = 0x08,
+  Okurigana = 0x10,
+  Meaning = 0x20,
+}
+
+export default WordProperties;
