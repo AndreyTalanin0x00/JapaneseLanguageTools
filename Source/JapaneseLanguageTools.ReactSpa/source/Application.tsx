8d3e0b707a86aafc8dc9b5802415a9ea332437cb
Andrey Talanin <andrey.talanin@outlook.com>
2026-04-16 19:33:36 +0200

    Update the Application React.js component (add ApplicationPreferencesContext context provider)

--- Source/JapaneseLanguageTools.ReactSpa/source/Application.tsx
+++ Source/JapaneseLanguageTools.ReactSpa/source/Application.tsx
@@ -1,28 +1,52 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import ApplicationLayout from "@/ApplicationLayout";
 import * as ApplicationSettings from "@/ApplicationSettings";
 import * as ApplicationSettingsMobile from "@/ApplicationSettings.Mobile";
 import isMobileBrowser from "@/isMobileBrowser";
+import * as ApplicationPreferencesConstants from "@/constants/ApplicationPreferencesConstants";
+import * as ObjectConstants from "@/constants/ObjectConstants";
 import MobileBrowserContext from "@/contexts/MobileBrowserContext";
+import ApplicationPreferencesContext from "@/contexts/ApplicationPreferencesContext";
+import ApplicationPreferences from "@/entities/preferences/ApplicationPreferences";
 
 const applicationPageDescriptors = ApplicationSettings.applicationPageDescriptors.filter((descriptor) => !descriptor.disabled);
 const applicationMenuItemDescriptors = ApplicationSettings.applicationMenuItemDescriptors.filter((descriptor) => !descriptor.disabled);
 const applicationMenuItemDescriptorsMobile = ApplicationSettingsMobile.applicationMenuItemDescriptors.filter((descriptor) => !descriptor.disabled);
 const applicationBreadcrumbItemDescriptors = ApplicationSettings.applicationBreadcrumbItemDescriptors;
 
+const applicationPreferencesInitialValues = localStorage.getItem(ApplicationPreferencesConstants.applicationPreferencesLocalStorageKey)
+  ? (JSON.parse(localStorage.getItem(ApplicationPreferencesConstants.applicationPreferencesLocalStorageKey) ?? ObjectConstants.emptyObjectString) as ApplicationPreferences)
+  : ApplicationPreferencesConstants.applicationPreferencesDefaultValues;
+
 const Application = () => {
   const mobileBrowserMode = useMemo(() => isMobileBrowser(), []);
 
+  const [applicationPreferences, setApplicationPreferences] = useState<ApplicationPreferences>(applicationPreferencesInitialValues);
+
+  useEffect(() => {
+    localStorage.setItem(ApplicationPreferencesConstants.applicationPreferencesLocalStorageKey, JSON.stringify(applicationPreferences));
+  }, [applicationPreferences]);
+
+  useEffect(() => {
+    if (applicationPreferences == ApplicationPreferencesConstants.applicationPreferencesDefaultValues) {
+      // If the default object was used, there are no preferences in the local storage; save them once.
+      setApplicationPreferences({ ...applicationPreferences });
+    }
+  }, [applicationPreferences]);
+
+  // prettier-ignore
   return (
     <BrowserRouter>
       <MobileBrowserContext.Provider value={mobileBrowserMode}>
-        <ApplicationLayout
-          applicationPageDescriptors={applicationPageDescriptors}
-          applicationMenuItemDescriptors={mobileBrowserMode ? applicationMenuItemDescriptorsMobile : applicationMenuItemDescriptors}
-          applicationBreadcrumbItemDescriptors={applicationBreadcrumbItemDescriptors}
-        />
+        <ApplicationPreferencesContext.Provider value={{ applicationPreferences, setApplicationPreferences }}>
+          <ApplicationLayout
+            applicationPageDescriptors={applicationPageDescriptors}
+            applicationMenuItemDescriptors={mobileBrowserMode ? applicationMenuItemDescriptorsMobile : applicationMenuItemDescriptors}
+            applicationBreadcrumbItemDescriptors={applicationBreadcrumbItemDescriptors}
+          />
+        </ApplicationPreferencesContext.Provider>
       </MobileBrowserContext.Provider>
     </BrowserRouter>
   );
