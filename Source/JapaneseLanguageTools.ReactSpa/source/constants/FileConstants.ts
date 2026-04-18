87d96c15d379ecd743316c002b998afd81e2fa9d
Andrey Talanin <andrey.talanin@outlook.com>
2026-04-18 02:29:26 +0200

    Add the 'FileConstants.ts' TypeScript constants file

---
+++ Source/JapaneseLanguageTools.ReactSpa/source/constants/FileConstants.ts
@@ -0,0 +1,4 @@
+export const maxImportFileSizeBytes = 16 * 1024 * 1024; // 16 MiB
+
+export const applicationDictionaryImportUploadAccept = ".json,.xml,.zip";
+export const tagImportUploadAccept = ".json,.xml,.zip";
