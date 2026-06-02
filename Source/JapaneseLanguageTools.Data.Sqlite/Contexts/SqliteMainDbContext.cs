f6d482fb14acd3b2d7f723324b5f35422bf6430f
Andrey Talanin <andrey.talanin@outlook.com>
2026-06-02 23:39:33 +0200

    Add the WordGroupHierarchyRecords database sets to the SqliteMainDbContext database context class

--- Source/JapaneseLanguageTools.Data.Sqlite/Contexts/SqliteMainDbContext.cs
+++ Source/JapaneseLanguageTools.Data.Sqlite/Contexts/SqliteMainDbContext.cs
@@ -340,6 +340,37 @@ public class SqliteMainDbContext : MainDbContext
             });
         });
 
+        modelBuilder.Entity<WordGroupHierarchyRecord>(entityBuilder =>
+        {
+            entityBuilder.HasKey(entity => new { entity.WordGroupId, entity.NestedWordGroupId });
+
+            entityBuilder
+                .HasOne(entity => entity.WordGroup)
+                .WithMany(entity => entity.WordGroupHierarchyRecords)
+                .HasForeignKey(entity => entity.WordGroupId)
+                .OnDelete(DeleteBehavior.Cascade);
+            entityBuilder
+                .HasOne(entity => entity.NestedWordGroup)
+                .WithMany()
+                .HasForeignKey(entity => entity.NestedWordGroupId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entityBuilder
+                .HasIndex(entity => entity.WordGroupId)
+                .HasDatabaseName("IX_WordGroupHierarchyRecord_WordGroupId");
+            entityBuilder
+                .HasIndex(entity => entity.NestedWordGroupId)
+                .HasDatabaseName("IX_WordGroupHierarchyRecord_NestedWordGroupId");
+
+            entityBuilder
+                .Property(entity => entity.PreventRecursiveIncludes)
+                .HasDefaultValue(false);
+
+            entityBuilder.ToTable("WordGroupHierarchyRecord", tableBuilder =>
+            {
+            });
+        });
+
         modelBuilder.Entity<WordExercise>(entityBuilder =>
         {
             entityBuilder.HasKey(entity => entity.Id);
