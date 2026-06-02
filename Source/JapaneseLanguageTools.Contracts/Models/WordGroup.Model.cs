e317f9b30959cbc671b1794ec75675151a931672
Andrey Talanin <andrey.talanin@outlook.com>
2026-06-03 00:27:38 +0200

    Add and update the WordGroupHierarchyRecordModel, WordGroupModel application-level model classes

--- Source/JapaneseLanguageTools.Contracts/Models/WordGroup.Model.cs
+++ Source/JapaneseLanguageTools.Contracts/Models/WordGroup.Model.cs
@@ -29,5 +29,7 @@ public class WordGroupModel
     [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
     public DateTimeOffset UpdatedOn { get; set; }
 
+    public IList<WordGroupHierarchyRecordModel> WordGroupHierarchyRecords { get; set; } = [];
+
     public IList<WordModel> Words { get; set; } = [];
 }
