56e642fa5cb4576edd3dce30930d6a51e169fb37
Andrey Talanin <andrey.talanin@outlook.com>
2026-06-04 00:38:10 +0200

    Refactor the ImportApplicationDictionaryRequestModel, ImportTagsRequestModel application-level request model classes (move properties down from the base ImportRequestModel class)

--- Source/JapaneseLanguageTools.Contracts/Models/Requests/Base/ImportRequest.Model.cs
+++ Source/JapaneseLanguageTools.Contracts/Models/Requests/Base/ImportRequest.Model.cs
@@ -1,8 +1,5 @@
-using JapaneseLanguageTools.Contracts.Models.Blobs;
-
 namespace JapaneseLanguageTools.Contracts.Models.Requests.Base;
 
 public class ImportRequestModel
 {
-    public required BlobReferenceModel[] BlobReferences { get; set; } = [];
 }
