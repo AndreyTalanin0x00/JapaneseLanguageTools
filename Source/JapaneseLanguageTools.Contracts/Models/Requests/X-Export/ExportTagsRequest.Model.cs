5d1d4a823fd54311bffcd89da0f8a173434ff270
Andrey Talanin <andrey.talanin@outlook.com>
2026-06-04 00:37:32 +0200

    Refactor the ExportApplicationDictionaryRequestModel, ExportTagsRequestModel application-level request model classes (move properties down from the base ExportRequestModel class)

--- Source/JapaneseLanguageTools.Contracts/Models/Requests/X-Export/ExportTagsRequest.Model.cs
+++ Source/JapaneseLanguageTools.Contracts/Models/Requests/X-Export/ExportTagsRequest.Model.cs
@@ -1,3 +1,4 @@
+using JapaneseLanguageTools.Contracts.Enumerations;
 using JapaneseLanguageTools.Contracts.Models.Requests.Base;
 
 // Use the IDE0079 (Remove unnecessary suppression) suppression (a Visual Studio false positive).
@@ -10,4 +11,7 @@ namespace JapaneseLanguageTools.Contracts.Models.Requests;
 
 public class ExportTagsRequestModel : ExportRequestModel
 {
+    public required SnapshotType SnapshotType { get; set; }
+
+    public required SnapshotFileFormat SnapshotFileFormat { get; set; }
 }
