e53ca052909bf8ccb277df839853d90e5349b2ee
Andrey Talanin <andrey.talanin@outlook.com>
2026-06-04 00:56:17 +0200

    Update the ExportApplicationDictionaryRequestModel, ExportTagsRequestModel TypeScript request model interfaces (add the zeroIdProperties property)

--- Source/JapaneseLanguageTools.ReactSpa/source/models/requests/x-export/ExportApplicationDictionaryRequest.Model.ts
+++ Source/JapaneseLanguageTools.ReactSpa/source/models/requests/x-export/ExportApplicationDictionaryRequest.Model.ts
@@ -5,4 +5,5 @@ import ExportRequestModel from "@/models/requests/base/ExportRequest.Model";
 export default interface ExportApplicationDictionaryRequestModel extends ExportRequestModel {
   snapshotType: SnapshotType;
   snapshotFileFormat: SnapshotFileFormat;
+  zeroIdProperties: boolean;
 }
--- Source/JapaneseLanguageTools.ReactSpa/source/models/requests/x-export/ExportTagsRequest.Model.ts
+++ Source/JapaneseLanguageTools.ReactSpa/source/models/requests/x-export/ExportTagsRequest.Model.ts
@@ -5,4 +5,5 @@ import ExportRequestModel from "@/models/requests/base/ExportRequest.Model";
 export default interface ExportTagsRequestModel extends ExportRequestModel {
   snapshotType: SnapshotType;
   snapshotFileFormat: SnapshotFileFormat;
+  zeroIdProperties: boolean;
 }
