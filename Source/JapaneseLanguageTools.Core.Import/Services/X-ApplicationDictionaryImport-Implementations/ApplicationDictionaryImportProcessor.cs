4409045e041091bb5d84069ac02e6df48c9cba3c
Andrey Talanin <andrey.talanin@outlook.com>
2026-06-06 16:47:39 +0200

    Update the ApplicationDictionaryImportProcessor service class (use the TagSeparator, AltTagSeparator constants)

--- Source/JapaneseLanguageTools.Core.Import/Services/X-ApplicationDictionaryImport-Implementations/ApplicationDictionaryImportProcessor.cs
+++ Source/JapaneseLanguageTools.Core.Import/Services/X-ApplicationDictionaryImport-Implementations/ApplicationDictionaryImportProcessor.cs
@@ -43,6 +43,11 @@ namespace JapaneseLanguageTools.Core.Import.Services;
 public class ApplicationDictionaryImportProcessor :
     IImportProcessor<ApplicationDictionaryImportRequest, ApplicationDictionaryImportResponse, Object, ApplicationDictionaryObjectPackageIntegrationModel>
 {
+    private static readonly string[] s_characterTagSeparators =
+        [CharacterIntegrationModel.TagSeparator, CharacterIntegrationModel.AltTagSeparator];
+    private static readonly string[] s_wordTagSeparators =
+        [WordIntegrationModel.TagSeparator, WordIntegrationModel.AltTagSeparator];
+
     private readonly IMapper m_mapper;
     private readonly IApplicationDictionaryService m_applicationDictionaryService;
     private readonly ITagService m_tagService;
@@ -176,7 +181,7 @@ public class ApplicationDictionaryImportProcessor :
         {
             CharacterId characterId = new(characterModel.Id);
 
-            characterModel.CharacterTags = MapTagString(characterIntegrationModel.Tags, updatedTagModelsByCaption).ToList();
+            characterModel.CharacterTags = MapTagString(characterIntegrationModel.Tags, updatedTagModelsByCaption, s_characterTagSeparators).ToList();
 
             switch (characterIntegrationModel.Action)
             {
@@ -226,7 +231,7 @@ public class ApplicationDictionaryImportProcessor :
             CharacterGroupId characterGroupId = new(characterGroupModel.Id);
 
             foreach ((CharacterModel characterModel, CharacterIntegrationModel characterIntegrationModel) in characterGroupModel.Characters.Zip(characterGroupIntegrationModel.Characters))
-                characterModel.CharacterTags = MapTagString(characterIntegrationModel.Tags, updatedTagModelsByCaption).ToList();
+                characterModel.CharacterTags = MapTagString(characterIntegrationModel.Tags, updatedTagModelsByCaption, s_characterTagSeparators).ToList();
 
             CharacterGroupModel? addedCharacterGroupModel = null;
             switch (characterGroupIntegrationModel.Action)
@@ -354,7 +359,7 @@ public class ApplicationDictionaryImportProcessor :
         {
             WordId wordId = new(wordModel.Id);
 
-            wordModel.WordTags = MapTagString(wordIntegrationModel.Tags, updatedTagModelsByCaption).ToList();
+            wordModel.WordTags = MapTagString(wordIntegrationModel.Tags, updatedTagModelsByCaption, s_wordTagSeparators).ToList();
 
             switch (wordIntegrationModel.Action)
             {
@@ -404,7 +409,7 @@ public class ApplicationDictionaryImportProcessor :
             WordGroupId wordGroupId = new(wordGroupModel.Id);
 
             foreach ((WordModel wordModel, WordIntegrationModel wordIntegrationModel) in wordGroupModel.Words.Zip(wordGroupIntegrationModel.Words))
-                wordModel.WordTags = MapTagString(wordIntegrationModel.Tags, updatedTagModelsByCaption).ToList();
+                wordModel.WordTags = MapTagString(wordIntegrationModel.Tags, updatedTagModelsByCaption, s_wordTagSeparators).ToList();
 
             WordGroupModel? addedWordGroupModel = null;
             switch (wordGroupIntegrationModel.Action)
@@ -516,11 +521,13 @@ public class ApplicationDictionaryImportProcessor :
         }
     }
 
-    private static IEnumerable<TagModel> MapTagString(string? tags, Dictionary<string, TagModel> updatedTagModelsByCaption)
+    private static IEnumerable<TagModel> MapTagString(string? tags, Dictionary<string, TagModel> updatedTagModelsByCaption, string[] tagSeparators)
     {
         tags ??= string.Empty;
 
-        string[] tagCaptions = tags.Split([';', ','], StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        StringSplitOptions tagSplitOptions = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
+
+        string[] tagCaptions = tags.Split(tagSeparators, tagSplitOptions);
 
         foreach (string tagCaption in tagCaptions)
         {
